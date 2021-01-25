73d53af54f49d855bb3e0ca059ea7a7ec2e4b6a4
Rikus <rpoolman@retrorabbit.co.za>
2021-01-25 15:54:50 +0200

    Pulling BTC price
    implemented the ability to pull the BTC price from coingeckp and display on the site

---
+++ GradDemo.Api/Controllers/CryptoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GradDemo.Api.Controllers
+{
+    public class CryptoController
+    {
+        [HttpGet("cryptoShit")]
+        public string GetCrypto() {
+            string outp = "";
+
+            WebRequest request = WebRequest.Create(
+             "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=USD");
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            // Get the stream containing content returned by the server.
+            // The using block ensures the stream is automatically closed.
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                outp = reader.ReadToEnd();
+                // Display the content.
+                Console.WriteLine(outp);
+            }
+
+            // Close the response.
+            response.Close();
+
+            return outp;
+        }
+
+    }
+}
