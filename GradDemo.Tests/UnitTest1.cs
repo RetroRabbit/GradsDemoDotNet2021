3257cd95620b953386ead6a43b83eb0304ef9457
David Noakes <dnoakes@retrorabbit.co.za>
2021-01-25 10:22:43 +0200

    Call helper

--- GradDemo.Tests/UnitTest1.cs
+++ GradDemo.Tests/UnitTest1.cs
@@ -1,12 +1,18 @@
 using NUnit.Framework;
+using System.Net.Http;
 
 namespace GradDemo.Tests
 {
     public class Tests
     {
+        private APIWebApplicationFactory? _apiWebApplicationFactory;
+        private HttpClient _httpClient;
+
         [SetUp]
         public void Setup()
         {
+            _apiWebApplicationFactory = new APIWebApplicationFactory();
+            _httpClient = _apiWebApplicationFactory.CreateClient();
         }
 
         [Test]
