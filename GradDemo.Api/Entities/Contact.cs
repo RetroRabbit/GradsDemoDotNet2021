d343221da25ed6401bf2fbc27171bb1ee5f7368f
David Noakes <dnoakes@retrorabbit.co.za>
2021-01-25 13:31:21 +0200

    Migrations

---
+++ GradDemo.Api/Entities/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradDemo.Api.Entities
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(100)]
+        public string LastName { get; set; }
+        [MaxLength(20)]
+        public string ContactNumber { get; set; }
+    }
+}
---
+++ GradDemo.Api/Migrations/20210125112718_added lastname field.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GradDemo.Api.Migrations
+{
+    public partial class addedlastnamefield : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Contacts",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Contacts");
+        }
+    }
+}
