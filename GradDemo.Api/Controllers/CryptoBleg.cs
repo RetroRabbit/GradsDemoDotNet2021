b91bc8b98267a58f7bbcf18f412c4f771dfb636a
Michael Schmidt <mschmidt@retrorabbit.co.za>
2021-01-25 16:08:14 -0800

    CryptoController - Michael SChmidt
    Controller that fetches crypto price in specified currency using an id

---
+++ GradDemo.Api/Controllers/CryptoBleg.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GradDemo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CryptoController : ControllerBase
+    {
+        private static readonly HttpClient client;
+
+        static CryptoController()
+        {
+            client = new HttpClient()
+            {
+
+            };
+        }
+
+        // GET: api/<CryptoController>
+        [HttpGet("{id}/{vs_currency}")]
+        public async Task<string> GetPrice(string id, string vs_currency)
+        {
+            string path = string.Format("https://api.coingecko.com/api/v3/simple/price?ids={0}&vs_currencies={1}", id, vs_currency);
+
+            HttpResponseMessage response = await client.GetAsync(path);
+
+            return await response.Content.ReadAsStringAsync();
+
+        }
+
+        // GET api/<CryptoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CryptoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CryptoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CryptoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
