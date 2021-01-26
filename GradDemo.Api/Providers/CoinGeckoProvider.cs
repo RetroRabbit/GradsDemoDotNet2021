ce8022a50a23b33282f4df9cfcf39bf58eea8941
David Noakes <dnoakes@retrorabbit.co.za>
2021-01-26 10:20:47 +0200

    Made a CoinGeckoProvider

---
+++ GradDemo.Api/Providers/CoinGeckoProvider.cs
@@ -0,0 +1,42 @@
+﻿using GradDemo.Api.Models.CoinGecko;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GradDemo.Api.Providers
+{
+    public class CoinGeckoProvider
+    {
+        static HttpClient client = new HttpClient();
+
+        public async Task<double?> GetValueForCoin(string coinId, string currency)
+        {
+            double? resultValue = null;
+
+            string url = $"https://api.coingecko.com/api/v3/simple/price?ids={coinId}&vs_currencies={currency}";
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                string res = await response.Content.ReadAsStringAsync();
+
+                var coinGeckoResult = JsonConvert.DeserializeObject<CoinPrice>(res);
+
+                if (currency.Equals("zar", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    resultValue = coinGeckoResult.bitcoin.zar;
+                }
+                else if (currency.Equals("usd", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    resultValue = coinGeckoResult.bitcoin.usd;
+                }
+
+                return resultValue;
+            }
+
+            return null;
+        }
+    }
+}
